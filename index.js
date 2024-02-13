493a56b8b9d053dde075e779a92aa504e9d29bdd
KUNAL BAIRWA <156991223+kunalbairwa@users.noreply.github.com>
2024-02-13 19:18:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const display = document.getElementsByid("display")
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+    
+}  
+
+function calculate(){
+
+}
